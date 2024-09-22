70f33aaa87c7239330701bb43e72eab695ed6960
pjgb-git <paulajgarcia68@gmail.com>
2024-09-22 16:48:34 -0300

    22/09/24 subiendo a Github

---
+++ httptoken.php
@@ -0,0 +1,26 @@
+<?php
+/*TODO Token de acceso a tu BOT*/
+$botToken ="7686983061:AAHAXonp0DiTW4TZyBvt1daghZ6R_HarXDA";
+
+/*TODO url del webhook*/
+$webhookurl="https://paulajgarcia68.wixsite.com/chatbot-paula";
+
+/*TODO configura el webhook mediante una solicitud http*/
+$apiurl ="https://api.telegram.org/bot$botToken/setWebhook?url=$webhookurl";
+$response = file_get_contents($apiurl);
+
+/*TODO verifica la configuración del webhook esté bien*/
+if ($response === false){
+    $error = error_get_last();
+    echo "Error al configurar el webhook:".$error['message'];
+}else{
+    /*TODO verifica la respuesta de telegram*/
+    $responsedata =json_decode($response,true);
+    if($responsedata['ok']===true){
+        echo "wehbook configurado con exito";
+    }else{
+        echo "error";
+    }
+}
+
+?>
