2537526f5c7ddd3e6f85fb131482793c49d6edb7
pjgb-git <paulajgarcia68@gmail.com>
2024-11-07 21:52:40 -0300

    7nov21

--- httptoken.php
+++ httptoken.php
@@ -3,7 +3,7 @@
 $botToken ="7686983061:AAHAXonp0DiTW4TZyBvt1daghZ6R_HarXDA";
 
 /*TODO url del webhook*/
-$webhookurl="https://lavieinformatica.com";
+$webhookurl="http://lavieinformatica.com/index.php";
 
 /*TODO configura el webhook mediante una solicitud http*/
 $apiurl ="https://api.telegram.org/bot$botToken/setWebhook?url=$webhookurl";
